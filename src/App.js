2689457bef49c8e8d92789637f0894e3dfb5ef1e
mubex-dot <63114194+mubex-dot@users.noreply.github.com>
2022-10-23 21:10:58 +0100

    push to github successful

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ function App() {
     <div className="App">
       <header>
         <img src={profilepic} alt="" />
-        <h1>Me</h1>
-        <p>We</p>
         <p>us</p>
       </header>
     </div>
