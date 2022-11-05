57b2f785e40c58198dbc921140222253996d527c
mubex-dot <63114194+mubex-dot@users.noreply.github.com>
2022-11-05 21:44:01 +0100

    style: added hover and focus state to form input and button

--- src/pages/Contact/contact.js
+++ src/pages/Contact/contact.js
@@ -5,7 +5,7 @@ function Contact() {
   return (
     <div className="contactme" style={{ fontSize: "0.8rem" }}>
       <Form />
-      <Footer style={{ width: "50%" }} />
+      <Footer />
     </div>
   );
 }
