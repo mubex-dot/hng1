317d7ae2eff32672fcf4e6207bee38c4e2f0bced
mubex-dot <63114194+mubex-dot@users.noreply.github.com>
2023-01-04 23:05:33 +0100

    Update data.js
    Made changes to the order of arrangement of links

--- src/data.js
+++ src/data.js
@@ -1,21 +1,15 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default [
   {
-    id: "twitter",
-    desc: "Twitter Link",
-    url: "https://twitter.com/m_u_b_e_x_",
-    target: "_blank",
-  },
-  {
-    id: "whatsapp",
-    desc: "Whatsapp",
-    url: "https://api.whatsapp.com/qr/GT66T5TVSCXGO1",
-    target: "_blank",
-  },
-  {
     id: "linkedin",
     desc: "Linkedin",
     url: "https://www.linkedin.com/in/mubarak-ibrahim-b04684234/",
+    target: "_blank",
+  },
+  {
+    id: "twitter",
+    desc: "Twitter Link",
+    url: "https://twitter.com/m_u_b_e_x_",
     target: "_blank",
   },
   {
@@ -28,6 +22,12 @@ export default [
     id: "instagram",
     desc: "Instagram",
     url: "https://www.instagram.com/m_u_b_e_x_/",
+    target: "_blank",
+  },
+  {
+    id: "whatsapp",
+    desc: "Whatsapp",
+    url: "https://api.whatsapp.com/qr/GT66T5TVSCXGO1",
     target: "_blank",
   },
   {
